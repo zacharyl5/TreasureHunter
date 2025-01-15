d23e46708e72fa29aba6b2f10cbfa9c7a9d0f2b3
BT_1N3_03 <a@.a>
2025-01-15 09:58:11 -0500

    TH-10 hard mode shop bug

--- TreasureHunter.java
+++ TreasureHunter.java
@@ -67,11 +67,11 @@ public class TreasureHunter {
      * Creates a new town and adds the Hunter to it.
      */
     private void enterTown() {
-        double markdown = 0.25;
+        double markdown = 0.5;
         double toughness = 0.4;
         if (hardMode) {
             // in hard mode, you get less money back when you sell items
-            markdown = 0.5;
+            markdown = 0.25;
 
             // and the town is "tougher"
             toughness = 0.75;
